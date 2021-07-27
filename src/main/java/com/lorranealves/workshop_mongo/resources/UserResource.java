6d91865ac1aeb735bbdce7e1ae937d93f8ad521d
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-27 20:59:14 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/lorranealves/workshop_mongo/resources/UserResource.java
+++ src/main/java/com/lorranealves/workshop_mongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lorranealves.workshop_mongo.resources;
 
+import com.lorranealves.workshop_mongo.domain.Post;
 import com.lorranealves.workshop_mongo.domain.User;
 import com.lorranealves.workshop_mongo.dto.UserDTO;
 import com.lorranealves.workshop_mongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable  String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
