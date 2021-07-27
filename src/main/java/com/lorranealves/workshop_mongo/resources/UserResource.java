1e4291ac2db646d2fbb709e322beb18c3e895f98
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-27 20:10:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/lorranealves/workshop_mongo/resources/UserResource.java
+++ src/main/java/com/lorranealves/workshop_mongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.lorranealves.workshop_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +16,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResources {
+public class UserResource {
 
     @Autowired
     private UserService service;
@@ -30,5 +32,13 @@ public class UserResources {
     public ResponseEntity<UserDTO> findById(@PathVariable  String id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/lorranealves/workshop_mongo/services/UserService.java
+++ src/main/java/com/lorranealves/workshop_mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lorranealves.workshop_mongo.services;
 
 import com.lorranealves.workshop_mongo.domain.User;
+import com.lorranealves.workshop_mongo.dto.UserDTO;
 import com.lorranealves.workshop_mongo.repository.UserRepository;
 import com.lorranealves.workshop_mongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
