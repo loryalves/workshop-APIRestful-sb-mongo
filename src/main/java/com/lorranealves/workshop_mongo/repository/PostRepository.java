8c7cebb7fbdd3ad51d845b9f1964a5b558112e81
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-27 21:36:58 -0300

    Consulta simples com @Query

--- src/main/java/com/lorranealves/workshop_mongo/repository/PostRepository.java
+++ src/main/java/com/lorranealves/workshop_mongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.lorranealves.workshop_mongo.repository;
 import com.lorranealves.workshop_mongo.domain.Post;
 import com.lorranealves.workshop_mongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
