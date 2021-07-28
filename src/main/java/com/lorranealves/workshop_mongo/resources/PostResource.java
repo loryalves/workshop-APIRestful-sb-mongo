a946fe30c39e786e566f0c1b16f3d8183d02a2e2
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-27 21:29:49 -0300

    Consulta simples com query methods

--- src/main/java/com/lorranealves/workshop_mongo/resources/PostResource.java
+++ src/main/java/com/lorranealves/workshop_mongo/resources/PostResource.java
@@ -2,6 +2,7 @@ package com.lorranealves.workshop_mongo.resources;
 
 import com.lorranealves.workshop_mongo.domain.Post;
 import com.lorranealves.workshop_mongo.domain.Post;
+import com.lorranealves.workshop_mongo.resources.util.URL;
 import com.lorranealves.workshop_mongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +24,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
